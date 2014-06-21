49239d192f4275786745a02156c7133a3fefb072
Jake McCrary <jake@jakemccrary.com>
2014-06-21 09:25:43 -0500

    split testing tools into distinct profiles

--- project.clj
+++ project.clj
@@ -1,9 +1,16 @@
 (defproject example "0.1.0-SNAPSHOT"
+  :description "Test bed for trying different testing libraries."
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[expectations "2.0.6"]
-                                  [midje "1.6.3"]]
-                   :plugins [[lein-autoexpect "1.2.2"]
-                             [com.jakemccrary/lein-test-refresh "0.5.0"]]}
+  :profiles {:clojure-test {:plugins [[com.jakemccrary/lein-test-refresh "0.5.0"]]}
+
+             :midje {:dependencies [[midje "1.6.3"]]
+                     :plugins [[lein-midje "3.1.3"]]
+                     :test-paths ^:replace ["midje"]}
+
+             :expectations {:dependencies [[expectations "2.0.6"]]
+                            :plugins [[lein-autoexpect "1.2.2"]]
+                            :test-paths ^:replace ["expectations"]}
+             
              :speclj {:dependencies [[speclj "3.0.0"]]
                       :plugins [[speclj "3.0.0"]]
                       :test-paths ^:replace ["spec"]}})
