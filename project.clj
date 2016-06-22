b64bd1902de3b731eb2a05a1a292bfb21e8c97dd
Jake McCrary <jake@jakemccrary.com>
2016-06-22 09:35:41 -0500

    update versions

--- project.clj
+++ project.clj
@@ -1,20 +1,20 @@
 (defproject clojure-test-bed "0.1.0-SNAPSHOT"
   :description "Test bed for trying different testing libraries."
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.5.0"]]}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.15.0"]]}
 
-             :humane-test {:dependencies [[pjstadig/humane-test-output "0.6.0"]]
+             :humane-test {:dependencies [[pjstadig/humane-test-output "0.8.0"]]
                            :injections [(require 'pjstadig.humane-test-output)
                                         (pjstadig.humane-test-output/activate!)]}
 
-             :midje {:dependencies [[midje "1.6.3"]]
-                     :plugins [[lein-midje "3.1.3"]]
+             :midje {:dependencies [[midje "1.8.3"]]
+                     :plugins [[lein-midje "3.2"]]
                      :test-paths ^:replace ["midje"]}
 
-             :expectations {:dependencies [[expectations "2.0.7"]]
+             :expectations {:dependencies [[expectations "2.1.8"]]
                             :plugins [[lein-autoexpect "1.2.2"]]
                             :test-paths ^:replace ["expectations"]}
 
-             :speclj {:dependencies [[speclj "3.0.2"]]
-                      :plugins [[speclj "3.0.2"]]
+             :speclj {:dependencies [[speclj "3.3.2"]]
+                      :plugins [[speclj "3.3.2"]]
                       :test-paths ^:replace ["spec"]}})
